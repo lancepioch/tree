76f1e1feea83c7d7d2a300e3e958cee5a187a601
Lance Pioch <git@lance.sh>
2018-04-26 00:00:01 -0400

    Add projects and branches

---
+++ database/migrations/2018_04_25_202402_create_branches_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBranchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('issue_number');
+            $table->integer('forge_site_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branches');
+    }
+}
