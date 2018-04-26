d95e5dc609d5e555817b2577378b6b19dce6387b
Lance Pioch <git@lance.sh>
2018-04-26 19:54:12 -0400

    Set the webhook secret right before creation

--- app/Project.php
+++ app/Project.php
@@ -2,10 +2,15 @@
 
 namespace App;
 
+use App\Events\ProjectCreating;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+    protected $dispatchesEvents = [
+        'creating' => ProjectCreating::class,
+    ];
+
     protected $fillable = [
         'forge_site_url', 'forge_server_id', 'github_repo', 'webhook_secret', 'forge_deployment',
     ];
