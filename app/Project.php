881d26d0fdb4c86b98119ddd416ac300c7326ad4
Lance Pioch <git@lance.sh>
2018-04-26 01:52:21 -0400

    Add relationships

--- app/Project.php
+++ app/Project.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     //
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
 }
