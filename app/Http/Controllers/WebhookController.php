af96380360b747da7ae73118c7060e2f780f4f06
Lance Pioch <git@lance.sh>
2018-04-27 02:03:57 -0400

    Add the abillity to delete the sites via closing the pr

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\RemoveSite;
 use App\Jobs\SetupSite;
 use App\Project;
 use Illuminate\Http\Request;
@@ -11,17 +12,27 @@ class WebhookController extends Controller
     public function githubPullRequest(Request $request)
     {
         $input = $request->input();
-        switch ($input['action'] ?? 'other') {
-            case 'opened':
-                break;
-            default:
-                abort(200, 'Not Interested');
-        }
-
         $projects = Project::where('github_repo', $input['repository']['full_name'])->get();
 
-        foreach ($projects as $project) {
-            dispatch(new SetupSite($project, $input['pull_request']));
+        switch ($input['action'] ?? 'other') {
+            case 'opened':
+            case 'reopened':
+                foreach ($projects as $project)
+                    dispatch(new SetupSite($project, $input['pull_request']));
+                break;
+            case 'closed':
+                foreach ($projects as $project)
+                    dispatch(new RemoveSite($project, $input['pull_request']));
+                break;
+            case 'assigned':
+            case 'unassigned':
+            case 'review_requested':
+            case 'review_request_removed':
+            case 'labeled':
+            case 'unlabeled':
+            case 'edited':
+            default:
+                abort(200, 'Not Interested');
         }
     }
 }
---
+++ app/Jobs/RemoveSite.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Project;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Themsaid\Forge\Forge;
+
+class RemoveSite implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Project $project, $pullRequest)
+    {
+        $forge = new Forge($project->user->forge_token);
+
+        $branch = $project
+            ->branches()
+            ->where('issue_number', $pullRequest['number'])
+            ->orderBy('id', 'desc')
+            ->firstOrFail();
+
+        $forge->deleteMysqlUser($project->forge_server_id, $branch->forge_mysql_user_id);
+        $forge->deleteMysqlDatabase($project->forge_server_id, $branch->forge_mysql_database_id);
+        $forge->deleteSite($project->forge_server_id, $branch->forge_site_id);
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
