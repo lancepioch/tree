11bd2f7d53c5297a87c26b6313a3494fa81a9ba9
Lance Pioch <git@lance.sh>
2018-04-27 12:38:14 -0400

    Make sure github id is fillable

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'forge_token', 'github_token',
+        'name', 'email', 'forge_token', 'github_token', 'github_id'
     ];
 
     /**
