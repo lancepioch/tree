00058d9ca33ac6b2c0cf3762004471e822c77600
Lance Pioch <git@lance.sh>
2018-04-30 00:28:34 -0400

    Add project policy

---
+++ app/Policies/ProjectPolicy.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Project;
+use Github\Client;
+use Github\ResultPager;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Http\Request;
+
+class ProjectPolicy
+{
+    use HandlesAuthorization;
+
+    private $github;
+    private $request;
+
+    public function __construct(Client $github, Request $request)
+    {
+        $this->github = $github;
+        $this->request = $request;
+    }
+
+    private function administrateRepository(User $user, $repository)
+    {
+        $github = $this->github;
+
+        try {
+            $github->authenticate($user->github_token, null, Client::AUTH_HTTP_PASSWORD);
+
+            $paginator = new ResultPager($github);
+            $repositories = $paginator->fetchAll($github->api('me'), 'repositories', ['all']);
+        } catch (\Exception $exception) {
+            return false;
+        }
+
+        $repository = collect($repositories)
+            ->where('permissions.admin', true)
+            ->where('full_name', $repository);
+
+        return !empty($repository);
+    }
+
+    /**
+     * Determine whether the user can view the project.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Project  $project
+     * @return mixed
+     */
+    public function view(User $user, Project $project)
+    {
+        return $this->administrateRepository($user, $project->github_repo);
+    }
+
+    /**
+     * Determine whether the user can create projects.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        $repository = $this->request->get('github_repo');
+
+        return $this->administrateRepository($user, $repository);
+    }
+
+    /**
+     * Determine whether the user can update the project.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Project  $project
+     * @return mixed
+     */
+    public function update(User $user, Project $project)
+    {
+        return $this->administrateRepository($user, $project->github_repo);
+    }
+
+    /**
+     * Determine whether the user can delete the project.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Project  $project
+     * @return mixed
+     */
+    public function delete(User $user, Project $project)
+    {
+        return $this->administrateRepository($user, $project->github_repo);
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
+use App\Policies\ProjectPolicy;
+use App\Project;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        Project::class => ProjectPolicy::class,
     ];
 
     /**
