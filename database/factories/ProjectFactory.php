4c4c8f01fe6370622b7267ba9bea3b2033485aac
Lance Pioch <git@lance.sh>
2018-05-06 21:35:54 -0400

    Add factory for projects

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Project::class, function (Faker $faker) {
+    return [
+        'forge_site_url' => $faker->numberBetween(0, 1000) . '.' . $faker->domainWord . '.' . $faker->domainName,
+        'forge_server_id' => $faker->numberBetween(),
+        'github_repo' => $faker->domainWord . '/' . $faker->domainWord,
+        'webhook_secret' => $faker->sha1,
+        'webhook_id' => $faker->numberBetween(),
+        'forge_deployment' => "composer require\nphp artisan migrate",
+        'forge_deployment_initial' => 'php artisan key:generate',
+    ];
+});
