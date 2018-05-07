2a253dd2b4b015cd9c5943faa6ffe660463a439b
Lance Pioch <git@lance.sh>
2018-05-07 02:37:43 -0400

    Add home tests

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 
 class HomeTest extends TestCase
@@ -18,5 +19,36 @@ class HomeTest extends TestCase
     public function testHomeIndex()
     {
         $this->markTestIncomplete();
+    }
+
+    public function testHorizonAccess()
+    {
+        $user = factory(User::class)->create();
+        $anotherUser = factory(User::class)->create();
+
+        config(['app.admin' => $user->email]);
+
+        $response = $this->actingAs($user)
+            ->get("/horizon");
+
+        $response->assertStatus(200);
+
+        $response = $this->actingAs($anotherUser)
+            ->get("/horizon");
+
+        $response->assertStatus(403);
+    }
+
+    public function testLoginRedirect()
+    {
+        $response = $this->get("/home");
+        $response->assertRedirect('/login');
+
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get("/home");
+        $response->assertStatus(200);
+
     }
 }
