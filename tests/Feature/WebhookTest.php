0609e933b7068f4fd465787d242beda85cfe5566
Lance Pioch <git@lance.sh>
2018-05-07 12:38:30 -0400

    Add webhook controller test

--- tests/Feature/WebhookTest.php
+++ tests/Feature/WebhookTest.php
@@ -2,52 +2,213 @@
 
 namespace Tests\Feature;
 
+use App\Branch;
+use App\Jobs\DeploySite;
+use App\Jobs\RemoveInitialDeployment;
+use App\Jobs\RemoveSite;
+use App\Jobs\SetupSite;
+use App\Jobs\SetupSql;
+use App\Project;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Bus;
 use Tests\TestCase;
 
 class WebhookTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public function pr()
+    {
+        return [
+            'pull_request' => [
+
+            ],
+            'repository' => [
+                'full_name' => 'example/repository',
+            ],
+            'action' => 'none',
+        ];
+    }
+
+    public function headers($content = '', $secret = '')
+    {
+        $hash = hash_hmac('sha1', $content, $secret);
+
+        return [
+            'X-Hub-Signature' => 'sha1=' . $hash,
+        ];
+    }
+
     public function testNoPullRequest()
     {
-        $this->markTestIncomplete();
+        $pr = $this->pr();
+        unset($pr['pull_request']);
+
+        $response = $this->post('/api/webhooks/github/pullrequest', $pr, $this->headers());
+
+        $response->assertStatus(400);
     }
 
     public function testNoRepository()
     {
-        $this->markTestIncomplete();
+        $pr = $this->pr();
+        unset($pr['repository']);
+
+        $response = $this->post('/api/webhooks/github/pullrequest', $pr, $this->headers());
+
+        $response->assertStatus(400);
     }
 
     public function testNoSignature()
     {
-        $this->markTestIncomplete();
+        $response = $this->post('/api/webhooks/github/pullrequest', $this->pr(), []);
+
+        $response->assertStatus(400);
     }
 
     public function testSignatureVerification()
     {
-        $this->markTestIncomplete();
+        $project = factory(Project::class)->create(['user_id' => 1]);
+        $branch = factory(Branch::class)->make();
+        $project->branches()->save($branch);
+
+        $pr = $this->pr();
+        $pr['repository']['full_name'] = $project->github_repo;
+        $pr['pull_request']['number'] = $branch->issue_number;
+
+        $headers = $this->headers(json_encode($pr), $project->webhook_secret);
+
+        $response = $this->json('POST', '/api/webhooks/github/pullrequest', $pr, $headers);
+        $response->assertStatus(200);
+
+        $headers = $this->headers(json_encode($pr), 'not a working secret');
+        $response = $this->json('POST', '/api/webhooks/github/pullrequest', $pr, $headers);
+        $response->assertStatus(400);
     }
 
-    public function testOpened()
+    public function testNoProjectFound()
     {
-        $this->markTestIncomplete();
+        $project = factory(Project::class)->create(['user_id' => 1]);
+
+        $pr = $this->pr();
+        $pr['repository']['full_name'] = 'doesnotexist';
+        $pr['pull_request']['number'] = 1;
+
+        $headers = $this->headers(json_encode($pr), $project->webhook_secret);
+
+        $response = $this->json('POST', '/api/webhooks/github/pullrequest', $pr, $headers);
+
+        $response->assertStatus(400);
     }
 
-    public function testReopened()
+    public function testNoBranchFound()
     {
-        $this->markTestIncomplete();
+        Bus::fake();
+
+        $project = factory(Project::class)->create(['user_id' => 1]);
+        $branch = factory(Branch::class)->make();
+        $project->branches()->save($branch);
+
+        $pr = $this->pr();
+        $pr['repository']['full_name'] = $project->github_repo;
+        $pr['pull_request']['number'] = -1;
+
+        $pr['action'] = 'closed';
+        $headers = $this->headers(json_encode($pr), $project->webhook_secret);
+        $response = $this->json('POST', '/api/webhooks/github/pullrequest', $pr, $headers);
+        $response->assertStatus(400);
+        Bus::assertNotDispatched(RemoveSite::class);
+
+        $pr['action'] = 'synchronize';
+        $headers = $this->headers(json_encode($pr), $project->webhook_secret);
+        $response = $this->json('POST', '/api/webhooks/github/pullrequest', $pr, $headers);
+        $response->assertStatus(400);
+        Bus::assertNotDispatched(DeploySite::class);
+    }
+
+    public function testOpenedOrReopened()
+    {
+        Bus::fake();
+
+        $project = factory(Project::class)->create(['user_id' => 1]);
+        $branch = factory(Branch::class)->make();
+        $project->branches()->save($branch);
+
+        $pr = $this->pr();
+        $pr['repository']['full_name'] = $project->github_repo;
+        $pr['pull_request']['number'] = $branch->issue_number;
+
+        $pr['action'] = 'opened';
+        $headers = $this->headers(json_encode($pr), $project->webhook_secret);
+        $response = $this->json('POST', '/api/webhooks/github/pullrequest', $pr, $headers);
+        $response->assertStatus(200);
+        Bus::assertDispatched(SetupSite::class);
+
+        $pr['action'] = 'reopened';
+        $headers = $this->headers(json_encode($pr), $project->webhook_secret);
+        $response = $this->json('POST', '/api/webhooks/github/pullrequest', $pr, $headers);
+        $response->assertStatus(200);
+        Bus::assertDispatched(SetupSite::class);
     }
 
     public function testClosed()
     {
-        $this->markTestIncomplete();
+        Bus::fake();
+
+        $project = factory(Project::class)->create(['user_id' => 1]);
+        $branch = factory(Branch::class)->make();
+        $project->branches()->save($branch);
+
+        $pr = $this->pr();
+        $pr['repository']['full_name'] = $project->github_repo;
+        $pr['pull_request']['number'] = $branch->issue_number;
+
+        $pr['action'] = 'closed';
+        $headers = $this->headers(json_encode($pr), $project->webhook_secret);
+        $response = $this->json('POST', '/api/webhooks/github/pullrequest', $pr, $headers);
+        $response->assertStatus(200);
+        Bus::assertDispatched(RemoveSite::class);
     }
 
     public function testSynchronize()
     {
-        $this->markTestIncomplete();
+        Bus::fake();
+
+        $project = factory(Project::class)->create(['user_id' => 1]);
+        $branch = factory(Branch::class)->make();
+        $project->branches()->save($branch);
+
+        $pr = $this->pr();
+        $pr['repository']['full_name'] = $project->github_repo;
+        $pr['pull_request']['number'] = $branch->issue_number;
+
+        $pr['action'] = 'synchronize';
+        $headers = $this->headers(json_encode($pr), $project->webhook_secret);
+        $response = $this->json('POST', '/api/webhooks/github/pullrequest', $pr, $headers);
+        $response->assertStatus(200);
+        Bus::assertDispatched(DeploySite::class);
     }
 
     public function testOther()
     {
-        $this->markTestIncomplete();
+        Bus::fake();
+
+        $project = factory(Project::class)->create(['user_id' => 1]);
+        $branch = factory(Branch::class)->make();
+        $project->branches()->save($branch);
+
+        $pr = $this->pr();
+        $pr['repository']['full_name'] = $project->github_repo;
+        $pr['pull_request']['number'] = $branch->issue_number;
+
+        $pr['action'] = 'notarealaction';
+        $headers = $this->headers(json_encode($pr), $project->webhook_secret);
+        $response = $this->json('POST', '/api/webhooks/github/pullrequest', $pr, $headers);
+        $response->assertStatus(200);
+        Bus::assertNotDispatched(SetupSite::class);
+        Bus::assertNotDispatched(RemoveSite::class);
+        Bus::assertNotDispatched(DeploySite::class);
+        Bus::assertNotDispatched(SetupSql::class);
+        Bus::assertNotDispatched(RemoveInitialDeployment::class);
     }
 }
