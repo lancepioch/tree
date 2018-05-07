3712f1b24555a9392160a16cfbf879f5a0b6195e
Lance Pioch <git@lance.sh>
2018-05-06 21:33:12 -0400

    Update user factory to the latest migration

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,8 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'forge_token' => $faker->sha256,
+        'github_token' => $faker->sha1,
+        'github_id' => $faker->numberBetween(),
     ];
 });
