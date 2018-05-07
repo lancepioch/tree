0106fe22ca94d74132864f5dd01d4a8bf46c961e
Lance Pioch <git@lance.sh>
2018-05-07 02:35:38 -0400

    Change status code to client error

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -15,15 +15,15 @@ class WebhookController extends Controller
         $input = $request->input();
         $signature = $request->header('X-Hub-Signature');
 
-        abort_unless(isset($input['pull_request']), 200, 'Not a Pull Request');
-        abort_unless(isset($input['repository']), 200, 'Not a Repository');
-        abort_if(is_null($signature), 200, 'Signature Required');
+        abort_unless(isset($input['pull_request']), 400, 'Not a Pull Request');
+        abort_unless(isset($input['repository']), 400, 'Not a Repository');
+        abort_if(is_null($signature) || !str_contains($signature, '='), 400, 'Signature Required');
 
         [$algorithm, $signature] = explode('=', $signature, 2);
         $pullRequest = $input['pull_request'];
 
         $project = Project::where('github_repo', $input['repository']['full_name'])->with(['branches', 'user'])->first();
-        abort_if($project === null, 200, 'Project Not Found');
+        abort_if($project === null, 400, 'Project Not Found');
         $branch = $project->branches()->where('issue_number', $pullRequest['number'])->orderBy('id', 'desc')->first();
 
         // Signature Verification
