ef84a7003b2c2167e3dd41670839c3c96a33ab22
Lance Pioch <git@lance.sh>
2018-05-06 21:50:25 -0400

    Add test skeletons

---
+++ tests/Feature/ProfileTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProfileTest extends TestCase
+{
+    public function testProfileUpdate()
+    {
+        $this->markTestIncomplete();
+    }
+}
---
+++ tests/Feature/WebhookTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class WebhookTest extends TestCase
+{
+    public function testNoPullRequest()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testNoRepository()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testNoSignature()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSignatureVerification()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testOpened()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testReopened()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testClosed()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testSynchronize()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testOther()
+    {
+        $this->markTestIncomplete();
+    }
+}
