8911d86b7b5606462f824a6fd4eb41b2826007d7
Lance Pioch <git@lance.sh>
2018-05-08 23:16:42 -0400

    Fix the auth routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@
 Route::get('/', 'HomeController@welcome');
 Route::get('/home', 'HomeController@index');
 
-Auth::routes();
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
 Route::redirect('/login', '/login/github', 302);
 
 Route::post('/user/update', 'ProfileController@update');
