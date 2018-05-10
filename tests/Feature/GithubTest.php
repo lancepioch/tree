66cf84c689eeacfae6a6adbfbdb695e2261b91c5
Lance Pioch <git@lance.sh>
2018-05-10 00:31:46 -0400

    Test the GitHub functions

---
+++ tests/Feature/GithubTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Branch;
+use App\Project;
+use App\User;
+use Github\Client;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class GithubTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testGithubStatus()
+    {
+        $user = factory(User::class)->create();
+        $user = \Mockery::mock($user)->makePartial();
+        $project = factory(Project::class)->create(['user_id' => $user->id]);
+        $branch = factory(Branch::class)->create(['project_id' => $project->id]);
+        $branch->project = $project;
+        $project->user = $user;
+
+        $github = \Mockery::mock(Client::class);
+        $github->shouldReceive('authenticate')->andReturn();
+        $github->shouldReceive('api->statuses->create');
+        $user->shouldReceive('githubClient')->once()->andReturn($github);
+
+        $branch->githubStatus('success', 'my description', '*.test.example.com');
+    }
+
+    public function testGithubComment()
+    {
+        $user = factory(User::class)->create();
+        $user = \Mockery::mock($user)->makePartial();
+        $project = factory(Project::class)->create(['user_id' => $user->id]);
+        $branch = factory(Branch::class)->create(['project_id' => $project->id]);
+        $branch->project = $project;
+        $project->user = $user;
+
+        $github = \Mockery::mock(Client::class);
+        $github->shouldReceive('authenticate')->andReturn();
+        $github->shouldReceive('api->comments->create');
+        $user->shouldReceive('githubClient')->once()->andReturn($github);
+
+        $branch->githubComment('comment body');
+    }
+}
