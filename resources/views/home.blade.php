412cc0afa53df8d938d8dcdbeaff1c5826d7aa8e
Lance Pioch <git@lance.sh>
2020-01-18 13:51:44 -0500

    Test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-10 mt-3">
+                <h1 style="text-align: center;">Test!</h1>
                 <div class="card">
                     <div class="card-header">Tokens</div>
 
