6cdb5ba0cf77b5b382143f5eaa1224f48b77023b
Lance Pioch <git@lance.sh>
2020-01-18 13:44:17 -0500

    This route is static, no need for a duplicate

--- config/services.php
+++ config/services.php
@@ -38,6 +38,6 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => env('GITHUB_CALLBACK_URL'),
+        'redirect' => env('APP_URL') . '/login/github/callback',
     ],
 ];
