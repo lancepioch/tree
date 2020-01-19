d6b9979b4d04aa759f6ecd61366d9aed41b4a461
Lance Pioch <git@lance.sh>
2020-01-19 03:43:50 -0500

    Add github login test

--- tests/Feature/GithubTest.php
+++ tests/Feature/GithubTest.php
@@ -7,6 +7,7 @@ use App\Project;
 use App\User;
 use Github\Client;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Socialite\Facades\Socialite;
 use Tests\TestCase;
 
 class GithubTest extends TestCase
@@ -45,5 +46,24 @@ class GithubTest extends TestCase
         $user->shouldReceive('githubClient')->once()->andReturn($github);
 
         $branch->githubComment('comment body');
+    }
+
+    public function testGithubLogin()
+    {
+        $social = new \Laravel\Socialite\Two\User();
+        $social->id = 1;
+        $social->name = 'name';
+        $social->email = 'email@example.com';
+        $social->token = 'token';
+
+        Socialite::shouldReceive('driver->user')->once()->andReturn($social);
+        $request = $this->get('/login/github/callback');
+        $request->assertRedirect();
+
+        $user = User::query()->where('github_id', 1)->first();
+        $this->assertNotNull($user);
+        $this->assertEquals($user->name, 'name');
+        $this->assertEquals($user->email, 'email@example.com');
+        $this->assertEquals($user->github_token, 'token');
     }
 }
