25890ecbc6acca4b441cc4ab25955820155283be
Lance Pioch <git@lance.sh>
2022-11-27 12:00:48 -0500

    Remove these incorrect docblocks

--- app/User.php
+++ app/User.php
@@ -11,21 +11,11 @@ class User extends Authenticatable
 {
     use Notifiable;
     use HasFactory;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    
     protected $fillable = [
         'name', 'email', 'forge_token', 'github_token', 'github_id',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
