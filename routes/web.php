8241e7907afb973a90f257d75f76518f9a9c26eb
Lance Pioch <git@lance.sh>
2022-11-28 21:18:29 -0500

    Remove extra space

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('welcome');
 
-
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 
