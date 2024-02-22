b589412a496a8c431af126ec3f98482f67290b64
Lance Pioch <git@lance.sh>
2024-02-21 21:29:26 -0500

    Remove sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Webhooks\GithubPullRequestController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +13,5 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::post('/webhooks/github/pullrequest', GithubPullRequestController::class)->name('webhooks.github.pullrequest');
