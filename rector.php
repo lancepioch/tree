c83a09286e0fece7815b21c1600e24b32f595304
Lance Pioch <git@lance.sh>
2024-02-22 21:15:17 -0500

    Add rector

---
+++ rector.php
@@ -0,0 +1,19 @@
+<?php
+
+use Rector\Config\RectorConfig;
+use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rector): void {
+    $rector->paths([
+        __DIR__ . '/app',
+    ]);
+
+    $rector->skip([
+        NullToStrictStringFuncCallArgRector::class,
+    ]);
+
+    $rector->sets([
+        LevelSetList::UP_TO_PHP_82,
+    ]);
+};
